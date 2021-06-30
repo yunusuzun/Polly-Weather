d1bcc8891f9830292650cc746f2ddf987c68067a
Yunus Uzun <>
2021-06-30 12:50:01 +0300

    App build

---
+++ Polly Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Polly Weather
+//
+//  Created by Yunus Uzun on 30.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
